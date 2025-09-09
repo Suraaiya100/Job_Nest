be78a2274d8c7b60879ea7c020d01c646640b8c2
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 00:13:33 +0600

    added cross css property

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,33 +1,36 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 const JobListing = () => {
-    const {isSearched, searchFilter}= useContext(AppContext)
+    const { isSearched, searchFilter } = useContext(AppContext)
     return (
-       <div>
-        {/*Side bar*/}
-
         <div>
-            {/*Search from hero component*/}
-            {
-                isSearched && (searchFilter.title !=='' || searchFilter.location !== '') && (
-                 <>
-                   <h3>Current Search</h3>
-                   <div>
-                    {searchFilter.title && (
-                        <span>
-                            {searchFilter.title} 
-                        </span>)}
-                    {searchFilter.location && (
-                        <span>
-                            {searchFilter.location} 
-                        </span>)}
-                   </div>
-                 </>
-                )
-            }
+            {/*Side bar*/}
 
+            <div>
+                {/*Search from hero component*/}
+                {
+                    isSearched && (searchFilter.title !== '' || searchFilter.location !== '') && (
+                        <>
+                            <h3>Current Search</h3>
+                            <div>
+                                {searchFilter.title && (
+                                    <span>
+                                        {searchFilter.title}
+                                        <img className="curson-pointer" src={assets.cross_icon} alt="" />
+                                    </span>)}
+                                {searchFilter.location && (
+                                    <span>
+                                        {searchFilter.location}
+                                        <img className="curson-pointer" src={assets.cross_icon} alt="" />
+                                    </span>)}
+                            </div>
+                        </>
+                    )
+                }
+
+            </div>
         </div>
-       </div>
     )
 };
 export default JobListing
