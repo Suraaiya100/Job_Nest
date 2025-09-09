97c38a16974d30199a331443d2b03f27c43ca115
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 22:56:26 +0600

    added css property to the icons

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -5,21 +5,21 @@ const Hero = () => {
        <div className="container 2xl:px-20 mx:auto my-10">
         <div className="bg-gradient-to-r from-purple-400 to-blue-400 text-white py-16 text-center mx-2 rounded-xl">
           <h2 className="text-2xl md:text-3xl lg:text-4xl font-medium mb-4">Discover Your Next Opportunity</h2>
-          <p>Find your dream job in tech, marketing, design and more</p>
-          <div>
-            <div>
-                <img src={assets.search_icon} alt="" />
+          <p className="mb-8 max-w-xl mx-auto text-sm font-light px-5">Find your dream job in tech, marketing, design and more</p>
+          <div className="flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto">
+            <div className="flex items-center">
+                <img className="h-4 sm:h-5" src={assets.search_icon} alt="" />
                 <input type='text'
                 placeholder="Search For jobs" 
                 className="max-sm:text-xs p-2 runded outline-none w-full "/>
             </div>
-            <div>
-                <img src={assets.location_icon} alt="" />
+            <div className="flex items-center">
+                <img className="h-4 sm:h-5" src={assets.location_icon} alt="" />
                 <input type='text'
                 placeholder="Search For location" 
                 className="max-sm:text-xs p-2 runded outline-none w-full "/>
             </div>
-            <button>Search</button>
+            <button className="bg-blue-600 px-6 py-2 rounded text-white m-1">Search</button>
           </div>
         </div>
        </div>
