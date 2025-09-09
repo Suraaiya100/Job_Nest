1cedd04ec4b17ab87f4c64bbdb2a97f1e4ca2ee6
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 03:01:26 +0600

    updated job title

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -4,9 +4,9 @@ const JobCard = ({ job }) => {
     return (
         <div className="border p-6 shadow rounded">
             <div className="flex justify-between items-center">
-                <img src={assets.company_icon} alt="" />
+                <img className="h-8" src={assets.company_icon} alt="" />
             </div>
-            <h4>{job.tittle}</h4>
+            <h4 className="font-medium text-xl mt-2">{job.title}</h4>
             <div>
                 <span>{job.location}</span>
                 <span>{job.level}</span>
