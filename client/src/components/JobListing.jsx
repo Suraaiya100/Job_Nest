e14a85d105cab028c171981c3f9a1b5b06885959
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 01:45:40 +0600

    css to the side bar

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -7,20 +7,20 @@ const JobListing = () => {
         <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
             {/*Side bar*/}
 
-            <div>
+            <div className="w-full lg:w-1/4 bg-white px-4">
                 {/*Search from hero component*/}
                 {
                     isSearched && (searchFilter.title !== '' || searchFilter.location !== '') && (
                         <>
-                            <h3>Current Search</h3>
-                            <div>
+                            <h3 className="font-medium text-lg mb-4 ">Current Search</h3>
+                            <div className="mb-4 text-gray-600">
                                 {searchFilter.title && (
-                                    <span>
+                                    <span className="inline-flex items-center bg-purple-50 bordor border-blue-200 px-4 py-1.5 rounded">
                                         {searchFilter.title}
                                         <img onClick={e=> setSearchFilter(prev=>({...prev,title:""}))} className="cursor-pointer" src={assets.cross_icon} alt="" />
                                     </span>)}
                                 {searchFilter.location && (
-                                    <span>
+                                    <span className="inline-flex items-center bg-blue-50 bordor border-purple-200 px-4 py-1.5 rounded">
                                         {searchFilter.location}
                                         <img onClick={e=> setSearchFilter(prev=>({...prev,location:""}))} className="cursor-pointer" src={assets.cross_icon} alt="" />
                                     </span>)}
