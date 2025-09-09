1ed53006e9cb4c676dd950027f9c878eac27eb2b
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 02:43:15 +0600

    css around the job details

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { assets } from "../assets/assets";
-const JobCard = ({job}) => {
+const JobCard = ({ job }) => {
     return (
-       <div>
-        <div>
-            <img src={assets.company_icon} alt="" />
+        <div className="border p-6 shadow rounded">
+            <div className="flex justify-between items-center">
+                <img src={assets.company_icon} alt="" />
+            </div>
+            <h4>{job.tittle}</h4>
+            <div>
+                <span>{job.location}</span>
+                <span>{job.level}</span>
+            </div>
+            <p dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}></p>
+            <div>
+                <button>Apply Now</button>
+                <button>Learn more</button>
+            </div>
         </div>
-        <h4>{job.tittle}</h4>
-        <div>
-            <span>{job.location}</span>
-            <span>{job.level}</span>
-        </div>
-        <p dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
-        <div>
-            <button>Apply Now</button>
-            <button>Learn more</button>
-        </div>
-       </div>
     )
 };
 export default JobCard
