e744b2e94220ad3df2b0c6481855daad9770e009
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 05:01:20 +0600

    added image

--- client/src/components/AppDownload.jsx
+++ client/src/components/AppDownload.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { assets } from "../assets/assets";
 const ApplyDownLoad = () => {
     return (
-       <div>ApplyDownLoad </div>
+        <div>
+            <div>
+                <div>
+                    <h1>Download Our App</h1>
+                    <div>
+                        <a href="#">
+                            <img src={assets.play_store} alt="" />
+                        </a>
+                        <a href="#">
+                            <img src={assets.app_store} alt="" />
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     )
 };
 export default ApplyDownLoad 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import JobListing from "../components/JobListing";
+import ApplyDownLoad from "../components/AppDownload";
 const Home = () => {
     return (
        <div>
