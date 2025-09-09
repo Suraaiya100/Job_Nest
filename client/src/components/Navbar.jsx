d40e593759fbd9f1687367f15925d90d7ccffb02
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 19:30:53 +0600

    feat: add Navbar component with logo display

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import assests from "../assets/assets"
+const Navbar = () => {
+    return (
+       <div>
+        <div>
+            <img src={assests.logo} alt="" />
+        </div>
+       </div>
+    )
+};
+export default Navbar
