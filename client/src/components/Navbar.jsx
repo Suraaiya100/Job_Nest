28e385c7c7aae6651984b6a4bad32f16d083f3d2
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 20:01:21 +0600

    coloured the login button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
        <div className='shadow py-4'>
         <div className="container px-4 2xl:px-20 mx-auto flex justify-between items-center">
             <img src={assets.logo} alt="" />
-            <div>
-                <button>Recruiter Login</button>
-                <button>Login</button>
+            <div classname ="flex gap-4 max-sm:text-xs">
+                <button className='text-gray-600'>Recruiter Login</button>
+                <button className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
             </div>
         </div>
        </div>
