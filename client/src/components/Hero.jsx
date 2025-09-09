53a2bf4b279d6be12c3d2930cee0a7aacf943458
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 21:49:04 +0600

    search bar

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Hero = () => {
+    return (
+       <div>
+        <div>
+          <h2>Over 10,000+ jobs to apply</h2>
+        </div>
+       </div>
+    )
+};
+export default Hero
