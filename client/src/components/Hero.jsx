17e821a0fba95824c53f761334529d69a1451c13
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 22:16:22 +0600

    added heading inside search bar

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { assets } from "../assets/assets";
 const Hero = () => {
     return (
        <div>
         <div>
-          <h2>Over 10,000+ jobs to apply</h2>
+          <h2>Discover Your Next Opportunity</h2>
+          <p>Find your dream job in tech, marketing, design and more</p>
+          <div>
+            <div>
+                <img src={assets.search_icon} alt="" />
+                <input type='text'
+                placeholder="Search For jobs" 
+                className="max-sm:text-xs p-2 runded outline-none w-full "/>
+            </div>
+          </div>
         </div>
        </div>
     )
