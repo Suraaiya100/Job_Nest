ed36212ddf9a5bc9b2c55a9d7973f4ddd3869a78
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 23:48:09 +0600

    imported useState

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useContext,useRef} from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 const Hero = () => {
-
+    const {setSearchFilter, setIsSearched}= useContext(AppContext)
     
+    const titleRef= useRef(null)
+    const locationRef= useRef(null)
+    const onSearch=()=>{
+        setSearchFilter({
+            title:titleRef.current.value,
+            location:locationRef.current.value,
+        })
+        setIsSearched(true)
+        console.log({ title:titleRef.current.value,
+            location:locationRef.current.value})
 
 
+    }
 
     return (
-       <div className="container 2xl:px-20 mx:auto my-10">
+       <div className="container 2xl:px-20 mx-auto my-10">
         <div className="bg-gradient-to-r from-purple-400 to-blue-400 text-white py-16 text-center mx-2 rounded-xl">
           <h2 className="text-2xl md:text-3xl lg:text-4xl font-medium mb-4">Discover Your Next Opportunity</h2>
           <p className="mb-8 max-w-xl mx-auto text-sm font-light px-5">Find your dream job in tech, marketing, design and more</p>
@@ -16,15 +28,19 @@ const Hero = () => {
                 <img className="h-4 sm:h-5" src={assets.search_icon} alt="" />
                 <input type='text'
                 placeholder="Search For jobs" 
-                className="max-sm:text-xs p-2 runded outline-none w-full "/>
+                className="max-sm:text-xs p-2 rounded outline-none w-full "
+                ref={titleRef}
+                />
             </div>
             <div className="flex items-center">
                 <img className="h-4 sm:h-5" src={assets.location_icon} alt="" />
                 <input type='text'
                 placeholder="Search For location" 
-                className="max-sm:text-xs p-2 runded outline-none w-full "/>
+                className="max-sm:text-xs p-2 rounded outline-none w-full "
+                ref={locationRef}
+                />
             </div>
-            <button className="bg-blue-600 px-6 py-2 rounded text-white m-1">Search</button>
+            <button onClick={onSearch} className="bg-blue-600 px-6 py-2 rounded text-white m-1">Search</button>
           </div>
         </div>
 
