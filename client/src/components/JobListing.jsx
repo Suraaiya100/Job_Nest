207845620df43aa0f4e8277ad39a13958d1470a6
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 00:10:14 +0600

    added search logic

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, {useContext} from "react";
+import { AppContext } from "../context/AppContext";
 const JobListing = () => {
+    const {isSearched, searchFilter}= useContext(AppContext)
     return (
        <div>
-        
+        {/*Side bar*/}
+
+        <div>
+            {/*Search from hero component*/}
+            {
+                isSearched && (searchFilter.title !=='' || searchFilter.location !== '') && (
+                 <>
+                   <h3>Current Search</h3>
+                   <div>
+                    {searchFilter.title && (
+                        <span>
+                            {searchFilter.title} 
+                        </span>)}
+                    {searchFilter.location && (
+                        <span>
+                            {searchFilter.location} 
+                        </span>)}
+                   </div>
+                 </>
+                )
+            }
+
+        </div>
        </div>
     )
 };
