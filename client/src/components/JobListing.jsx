84305346b86f3058971b850c848c99d52dc863c4
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 02:02:17 +0600

    added job catagory

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { assets } from "../assets/assets";
+import { assets, JobCategories } from "../assets/assets";
+
 const JobListing = () => {
     const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext)
     return (
@@ -32,7 +33,16 @@ const JobListing = () => {
                 <div>
                     <h4>Search by Catagories</h4>
                     <ul>
-                        
+                        {
+                            JobCategories.map((category, index) => (
+                                <li key={index}>
+                                    <input type="checkbox" name="" id="" />
+                                    {category}
+                                    
+                                </li>
+                            ))
+                        }
+
                     </ul>
                 </div>
 
