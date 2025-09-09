6f4ae2fa50ef0e927feac4de9be97cc9cfed01bb
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 22:45:54 +0600

    added css property

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { assets } from "../assets/assets";
 const Hero = () => {
     return (
        <div className="container 2xl:px-20 mx:auto my-10">
-        <div className="bg-gradient-to-r from-purple-400 to-blue-400">
-          <h2>Discover Your Next Opportunity</h2>
+        <div className="bg-gradient-to-r from-purple-400 to-blue-400 text-white py-16 text-center mx-2 rounded-xl">
+          <h2 className="text-2xl md:text-3xl lg:text-4xl font-medium mb-4">Discover Your Next Opportunity</h2>
           <p>Find your dream job in tech, marketing, design and more</p>
           <div>
             <div>
