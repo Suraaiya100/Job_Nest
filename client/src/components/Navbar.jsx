a6b178a7b97f404af0e45ea3d6c919136462b4c5
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 21:10:47 +0600

    adding link tag after log in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets"
 import {useClerk,UserButton,useUser} from "@clerk/clerk-react"
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
 
     const {openSignIn}= useClerk()
@@ -13,7 +13,12 @@ const Navbar = () => {
             <img src={assets.logo} alt="" />
             {
                 user
-                ? <div></div>
+                ? <div>
+                    <Link to={'/applications'}>Applied Jobs</Link>
+                    <p>|</p>
+                    <p>Hi, {user.firstName+" "+user.lastName}</p>
+                    <UserButton/>
+                </div>
                 :<div classname ="flex gap-4 max-sm:text-xs">
                 <button className='text-gray-600'>Recruiter Login</button>
                 <button onClick={e=> openSignIn()} className='bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full'>Login</button>
