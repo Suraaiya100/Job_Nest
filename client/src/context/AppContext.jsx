2e2ff3874950624a67d233b685b9fb0fa4737252
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-09 19:16:05 +0600

    feat: add AppContext and AppContextProvider for state management

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const AppContext = createContext()
+export const AppContextProvider = () =>{
+    const value ={
+
+    }
+    return(<AppContext.Provider value={value}>
+        {props.children}
+    </AppContext.Provider>)
+}
