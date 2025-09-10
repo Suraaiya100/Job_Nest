9dc674bf5965de6b934b30b76462cda3ae990d59
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-11 01:11:09 +0600

    added upper bar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,9 +5,9 @@ const Dashboard = () => {
     return (
         <div className='min-h-screen'>
             <div className="shadow py-4">
-                <div>
-                    <img src={assets.logo} alt=""/>
-                    <div>
+                <div className="px-5 flex justify-between items-center">
+                    <img className="max-sm:w-32 cursor-pointer" src={assets.logo} alt=""/>
+                    <div className="flex items-center gap-3">
                         <p>Welcome, JobNest</p>
                         <div>
                             <img src={assets.company_icon} alt=""/>
