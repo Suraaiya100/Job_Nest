a2d06b1cf7e54cee5e391325374fd4744ace13cc
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-11 01:05:59 +0600

    updated page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { assets } from "../assets/assets";
 const Dashboard = () => {
     return (
-        <div>
-            Dashboard
-            <Outlet/>
+        <div className='min-h-screen'>
+            <div>
+                <div>
+                    <img src={assets.logo} alt=""/>
+                    <div>
+                        <p>Welcome, JobNest</p>
+                        <div>
+                            <img src={assets.company_icon} alt=""/>
+                            <div>
+                                <ul>
+                                    <li>Dashboard</li>
+                                    <li>Manage Jobs</li>
+                                    <li>Add Job</li>
+                                    <li>View Applications</li>
+                                </ul>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
         </div>
     )
 }
