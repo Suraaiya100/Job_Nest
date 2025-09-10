6538d4f5c41d1d3b24182036ce8f0505bfdae6c3
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 12:41:27 +0600

    updated pagination

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -115,7 +115,7 @@ const JobListing = () => {
                 </div>
 
                 {/*Pagination*/}
-                {jobs.length > 0 && (
+                {filteredJobs.length > 0 && (
                     <div className="flex items-center justify-center space-x-2 mt-10">
                         <a href='#job-list'>
                             <img onClick={() => setCurrentPage(Math.max(currentPage - 1, 1))} src={assets.left_arrow_icon} alt='' />
