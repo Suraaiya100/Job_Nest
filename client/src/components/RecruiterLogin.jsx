7de9d74035b2d2c475f7866bf409e0b935f083b8
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 21:40:18 +0600

    added css to the button

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -11,24 +11,24 @@ import { assets } from "../assets/assets";
         <div className="absolute top-0 left-0 right-0 bottom-0 z-0 backdrop-blur-sm bg-black/30 flex justify-center items-center ">
           <form className="relative bg-white p-10 rounded-xl text-slate-500">
             <h1 className="text-center text-2xl text-neutral-700 font-medium">Recruiter {state}</h1>
-            <p>Welcome back! Please sign in to continue</p>
+            <p className="text-sm">Welcome back! Please sign in to continue</p>
              <>
-              <div>
+              <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
                 <img src={assets.person_icon} alt=''/>
-                <input onChange={e=> setName(e.target.value)} value={name} type='text' placeholder= 'Company Name' required/>
+                <input className="outline-none text-sm" onChange={e=> setName(e.target.value)} value={name} type='text' placeholder= 'Company Name' required/>
               </div>
 
-              <div>
+              <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
                 <img src={assets.email_icon} alt=''/>
-                <input onChange={e=> setEmail(e.target.value)} value={email} type='email' placeholder= 'Email Id' required/>
+                <input className="outline-none text-sm" onChange={e=> setEmail(e.target.value)} value={email} type='email' placeholder= 'Email Id' required/>
               </div>
 
-              <div>
+              <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
                 <img src={assets.lock_icon} alt=''/>
-                <input onChange={e=> setPassword(e.target.value)} value={password} type='password' placeholder= 'Password' required/>
+                <input className="outline-none text-sm" onChange={e=> setPassword(e.target.value)} value={password} type='password' placeholder= 'Password' required/>
               </div>
              </>
-             <button>
+             <button className="bg-blue-600 w-full text-white py-2 rounded-full">
                 {state === 'Login' ? 'login' : 'create account'}
              </button>
           </form>
