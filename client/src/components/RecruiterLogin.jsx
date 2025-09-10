827630a0471e5ecfc610dfffea1aa0064a04e8a1
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 20:46:27 +0600

    updated for login

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -1,4 +1,5 @@
  import React, { useState } from "react";
+import { assets } from "../assets/assets";
   const RecruiterLogin = () => {
     const [state, setState]=useState('Login')
     const [name, setName]= useState('')
@@ -10,6 +11,23 @@
         <div>
           <form action="">
             <h1>Recruiter {state}</h1>
+            <p>Welcome back! Please sign in to continue</p>
+             <>
+              <div>
+                <img src={assets.person_icon} alt=''/>
+                <imput onChange={e=> setName(e.target.value)} value={name} type='text' placeholder= 'Company Name' required/>
+              </div>
+
+              <div>
+                <img src={assets.email_icon} alt=''/>
+                <imput onChange={e=> setEmail(e.target.value)} value={email} type='email' placeholder= 'Email Id' required/>
+              </div>
+
+              <div>
+                <img src={assets.lock_icon} alt=''/>
+                <imput onChange={e=> setPassword(e.target.value)} value={password} type='text' Splaceholder= 'Password' required/>
+              </div>
+             </>
           </form>
         </div>
      
