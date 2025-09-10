afad1231b91fd792f08c157a5208b1426a113853
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 21:15:42 +0600

    colour changed

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -9,7 +9,7 @@ import { assets } from "../assets/assets";
     const [isTextDataSubmited, setIsTextDataSubmited]= useState(false)
     return (
         <div className="absolute top-0 left-0 right-0 bottom-0 z-0 backdrop-blur-sm bg-black/30 flex justify-center items-center ">
-          <form action="">
+          <form className="relative bg-white p-10 rounded-xl text-slate-500">
             <h1>Recruiter {state}</h1>
             <p>Welcome back! Please sign in to continue</p>
              <>
