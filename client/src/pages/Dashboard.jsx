dc7f452538f09feef805c31784da3ef9ce9a32f4
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-11 00:59:16 +0600

    add job to this page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div>
+            Dashboard
+            <Outlet/>
+        </div>
     )
 }
 export default Dashboard;
