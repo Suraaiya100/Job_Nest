1453b0df6a1c67436d4d7c8e54732c3470d42f02
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 21:32:50 +0600

    added heading

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -10,7 +10,7 @@ import { assets } from "../assets/assets";
     return (
         <div className="absolute top-0 left-0 right-0 bottom-0 z-0 backdrop-blur-sm bg-black/30 flex justify-center items-center ">
           <form className="relative bg-white p-10 rounded-xl text-slate-500">
-            <h1>Recruiter {state}</h1>
+            <h1 className="text-center text-2xl text-neutral-700 font-medium">Recruiter {state}</h1>
             <p>Welcome back! Please sign in to continue</p>
              <>
               <div>
