dc67d2fd906f72efbb70aeba8678a9dc9eea563e
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 22:16:09 +0600

    added sign up, login, css property

--- client/src/components/RecruiterLogin.jsx
+++ client/src/components/RecruiterLogin.jsx
@@ -13,10 +13,11 @@ import { assets } from "../assets/assets";
             <h1 className="text-center text-2xl text-neutral-700 font-medium">Recruiter {state}</h1>
             <p className="text-sm">Welcome back! Please sign in to continue</p>
              <>
+               {state !== 'Login' &&(
               <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
                 <img src={assets.person_icon} alt=''/>
                 <input className="outline-none text-sm" onChange={e=> setName(e.target.value)} value={name} type='text' placeholder= 'Company Name' required/>
-              </div>
+              </div>)}
 
               <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
                 <img src={assets.email_icon} alt=''/>
@@ -32,6 +33,14 @@ import { assets } from "../assets/assets";
              <button className="bg-blue-600 w-full text-white py-2 rounded-full">
                 {state === 'Login' ? 'login' : 'create account'}
              </button>
+             {
+                state === 'Login'
+                ?<p className="mt-5 text-center">Don't have an account? <span className="text-blue-600 cursor-pointer" onClick={()=> setState("Sign Up")}>Sign Up</span></p>
+                :<p className="mt-5 text-center">Already have an account? <span className="text-blue-600 cursor-pointer" onClick={()=> setState("Login")}>Login</span></p>
+             }
+            
+            
+
           </form>
         </div>
      
