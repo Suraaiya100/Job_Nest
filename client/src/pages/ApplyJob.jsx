9c265395ba62f6a81994d41d374278e38ca88cfc
Suraaiya Alam <your-github-suraaiyaalam@gmail.com>
2025-09-10 16:14:58 +0600

    added description and apply now button

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,4 +1,4 @@
-import React , { useContext, useEffect, useState }from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
@@ -6,69 +6,76 @@ import Loading from "../components/Loading";
 import Navbar from "../components/Navbar";
 import kconvert from 'k-convert'
 import moment from 'moment'
-
 const ApplyJob = () => {
-    const { id }= useParams()
-    const [JobData, setJobData]= useState(null)
-    const {jobs}= useContext(AppContext)
-    const fetchJob= async() => {
-         const data= jobs.filter(job => job._id === id)
-         if (data.length !== 0) {
+    const { id } = useParams()
+    const [JobData, setJobData] = useState(null)
+    const { jobs } = useContext(AppContext)
+    const fetchJob = async () => {
+        const data = jobs.filter(job => job._id === id)
+        if (data.length !== 0) {
             setJobData(data[0])
             console.log(data[0])
 
-         }
+        }
     }
     useEffect(() => {
-        if(jobs.length >0){
-             fetchJob()
+        if (jobs.length > 0) {
+            fetchJob()
         }
-        
 
-    },[id,jobs])
+
+    }, [id, jobs])
     return JobData ? (
         <>
-           <Navbar/>
-           <div className="min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto" > 
-            <div className="bg-white text-black rounded-lg w-full">
-                <div className="flex justify-center md:justify-between flex-wrap gap-8 px-14 mb-6 bg-purple-50 border border-sky-400 rounded-xl">
-                    <div>
-                        <img src={JobData.companyId.image} alt=''/>
-                        <div>
-                            <h1>{JobData.title}</h1>
-                            <div>
-                                <span>
-                                    <img src={assets.suitcase_icon} alt=''/>
-                                    {JobData.companyId.name}
-                                </span>
-                                <span>
-                                    <img src={assets.location_icon} alt=''/>
-                                    {JobData.location}
-                                </span>
-                                <span>
-                                    <img src={assets.person_icon} alt=""/>
-                                    {JobData.level}
-                                </span>
-                                <span>
-                                    <img src={assets.money_icon} alt=""/>
-                                    CTC:{kconvert.convertTo(JobData.salary)}
-                                </span>
+            <Navbar />
+            <div className="min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto" >
+                <div className="bg-white text-black rounded-lg w-full">
+                    <div className="flex justify-center md:justify-between flex-wrap gap-8 px-14 mb-6 bg-purple-50 border border-purple-400 rounded-xl">
+                        <div className="flex flex-col md:flex-row items-center">
+                            <img className="h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border" src={JobData.companyId.image} alt='' />
+                            <div className="text-center md:text-left text-neutral-700">
+                                <h1 className="text-2xl sm:text-4xl font-medium">{JobData.title}</h1>
+                                <div className="flex flex-row flex-wrap max-md:justify-center gap-y-2 gap-6 items-center text-gray-600 mt-2">
+                                    <span className="flex items-center gap-1">
+                                        <img src={assets.suitcase_icon} alt='' />
+                                        {JobData.companyId.name}
+                                    </span>
+                                    <span className="flex items-center gap-1">
+                                        <img src={assets.location_icon} alt='' />
+                                        {JobData.location}
+                                    </span>
+                                    <span className="flex items-center gap-1">
+                                        <img src={assets.person_icon} alt="" />
+                                        {JobData.level}
+                                    </span>
+                                    <span className="flex items-center gap-1">
+                                        <img src={assets.money_icon} alt="" />
+                                        CTC:{kconvert.convertTo(JobData.salary)}
+                                    </span>
+                                </div>
                             </div>
+                        </div>
+
+                        <div className="flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center">
+                            <button className="bg-blue-600 p-2.5 px-10 text-white rounded">Apply Now</button>
+                            <p className="mt-1 text-gray-600"> Posted {moment(JobData.date).fromNow()}</p>
                         </div>
                     </div>
 
-                         <div>
-                           <button>Apply Now</button>
-                           <p>Posted {moment (JobData.date).fromNow()}</p>
-                         </div>
+                    <div>
+                        <div>
+                            <h2>Job description</h2>
+                            <div dangerouslySetInnerHTML={{ __html: JobData.description }}/>
+                            <button>Apply Now</button>
+                        </div>
+                    </div>
 
                 </div>
             </div>
-           </div>
         </>
 
-    ):(
-       <Loading/>
+    ) : (
+        <Loading />
     )
 }
 export default ApplyJob
